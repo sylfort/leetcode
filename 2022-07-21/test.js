783819f3b184ae8ec4e41cfc8b6555d6a1dcc96f
Sylvio <sylviomafort@gmail.com>
2022-07-21 09:41:51 +0900

    2022-07-21 09:41

---
+++ 2022-07-21/test.js
@@ -0,0 +1,13 @@
+// 1. Two Sum
+// test
+var twoSum = function (nums, target) {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    let diff = target - nums[i];
+    if (diff in map) {
+      return [map[diff], i];
+    } else {
+      map[nums[i]] = i;
+    }
+  }
+};
